6bdc95581c010a868ae725f5bb52f067d7714209
FatCatCoder <christian75armand@gmail.com>
2024-06-02 12:24:25 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import PWABadge from './components/PWABadge.vue';
+import WavesurferComp from './components/WavesurferComp.vue';
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-indigo-500">Transcribe Online!</h1>
+  <div>
+    <WavesurferComp />
+  </div>
+  <HelloWorld msg="vite-project" />
+  <PWABadge />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: 'vite-project',
+        short_name: 'vite-project',
+        description: 'vite-project',
+        theme_color: '#ffffff',
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
+});
